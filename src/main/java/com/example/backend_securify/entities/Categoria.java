96334900fea804b7a360577da9a23d43975a1580
DAGA6542 <u202120374@upc.edu.pe>
2025-05-09 21:28:19 -0500

    Backend

---
+++ src/main/java/com/example/backend_securify/entities/Categoria.java
@@ -0,0 +1,24 @@
+package com.example.backend_securify.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private long idCategoria;
+    private String nombre;
+    private String descripcion;
+}
