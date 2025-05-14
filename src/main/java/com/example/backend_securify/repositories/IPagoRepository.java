917efc49deb3c6b4cd8f0e0f7f5d1bcc43b51b70
FernandoAElias0302 <u20211a797@upc.edu.pe>
2025-05-14 03:08:30 -0500

    nuevo cambio
    hola

--- src/main/java/com/example/backend_securify/repositories/IPagoRepository.java
+++ src/main/java/com/example/backend_securify/repositories/IPagoRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface IPagoRepository extends JpaRepository<Pago, Long> {
-    List<Pago> findPagoByMontoEquals(double precio);
 }
